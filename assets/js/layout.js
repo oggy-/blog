d9409d9b969ae9a6e97585f437bd9607a27d59d6
Ognjen Maric <ognjen.maric@dfinity.org>
2023-10-24 21:23:07 +0200

    Try again, hopefully a better experience on mobile

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,13 +1,26 @@
-window.addEventListener("DOMContentLoaded", function() {
-    var checkbox = document.getElementById('toc-toggle');
+document.addEventListener('DOMContentLoaded', adjustSlidebarPosition);
+window.addEventListener('resize', adjustSlidebarPosition);
+
+document.addEventListener('DOMContentLoaded', addToggleSidebarListener);
+
+function adjustSlidebarPosition() {
+    const headerHeight = document.querySelector('.site-header').offsetHeight;
+    const slidebar = document.getElementById('toc-wrapper');
+    slidebar.style.top = `${headerHeight}px`;
+}
+
+window.addEventListener("load", function() {
+    const slidebar = document.querySelector('.toc-fixed');
 
     // Function to set checkbox state based on window width
     function setCheckboxState() {
         if (window.innerWidth <= 768) { // Assuming 768px is the breakpoint
-            checkbox.checked = false;
-        } else {
-            checkbox.checked = true;
+            slidebar.classList.add('collapsed');
+            const contents = document.getElementById('non-toc-wrapper');
+            contents.style.left = `30px`;
+
         }
+
     }
 
     // Set initial state
@@ -16,3 +29,23 @@ window.addEventListener("DOMContentLoaded", function() {
     // Update state on window resize
     window.addEventListener("resize", setCheckboxState);
 });
+
+function addToggleSidebarListener() {
+    let toggle = document.getElementById('toc-toggle');
+    toggle.addEventListener('click', function() {
+        const slidebar = document.querySelector('.toc-fixed');
+        const contents = document.getElementById('non-toc-wrapper');
+        const slidebarWrapper = document.getElementById('toc-wrapper');
+        if (slidebar.classList.contains('collapsed')) {
+            slidebar.classList.remove('collapsed');
+            const offset = slidebarWrapper.offsetWidth + 10;
+            contents.style.left = `${offset}px`;
+        } else {
+            slidebar.classList.add('collapsed');
+            const offset = slidebarWrapper.offsetWidth + 10;
+            contents.style.left = `${offset}px`;
+        }
+
+    });
+}
+
