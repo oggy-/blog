3f6631a5bbf3ff3e697afc37f2dc0f64e3d286e6
Ognjen Maric <ognjen.maric@dfinity.org>
2023-10-24 21:58:03 +0200

    More fiddling with offsets, full-on JS solution now.

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -4,48 +4,56 @@ window.addEventListener('resize', adjustSlidebarPosition);
 document.addEventListener('DOMContentLoaded', addToggleSidebarListener);
 
 function adjustSlidebarPosition() {
-    const headerHeight = document.querySelector('.site-header').offsetHeight;
     const slidebar = document.getElementById('toc-wrapper');
-    slidebar.style.top = `${headerHeight}px`;
+    if(slidebar) {
+        const headerHeight = document.querySelector('.site-header').offsetHeight;
+        slidebar.style.top = `${headerHeight}px`;
+    }
 }
 
+function limitContentWidth() {
+    const contents = document.getElementById('non-toc-wrapper');
+    const slidebarWrapper = document.getElementById('toc-wrapper');
+    const offset = slidebarWrapper.offsetWidth + 10;
+    const maxWidth = window.innerWidth - offset;
+    contents.style.maxWidth = `${maxWidth}px`;
+    contents.style.paddingLeft = `${offset}px`;
+    if(window.innerWidth >= 768){
+        contents.style.paddingRight = `${offset}px`;
+    }
+}
 window.addEventListener("load", function() {
     const slidebar = document.querySelector('.toc-fixed');
-
-    // Function to set checkbox state based on window width
-    function setCheckboxState() {
-        if (window.innerWidth <= 768) { // Assuming 768px is the breakpoint
-            slidebar.classList.add('collapsed');
-            const contents = document.getElementById('non-toc-wrapper');
-            contents.style.left = `30px`;
+    if(slidebar) {
+        // Function to set checkbox state based on window width
+        function setCheckboxState() {
+            if (window.innerWidth <= 768) { // Assuming 768px is the breakpoint
+                slidebar.classList.add('collapsed');
+            }
+            limitContentWidth();
 
         }
+        // Set initial state
+        setCheckboxState();
 
+        // Update state on window resize
+        window.addEventListener("resize", setCheckboxState);
     }
-
-    // Set initial state
-    setCheckboxState();
-
-    // Update state on window resize
-    window.addEventListener("resize", setCheckboxState);
 });
 
 function addToggleSidebarListener() {
     let toggle = document.getElementById('toc-toggle');
-    toggle.addEventListener('click', function() {
-        const slidebar = document.querySelector('.toc-fixed');
-        const contents = document.getElementById('non-toc-wrapper');
-        const slidebarWrapper = document.getElementById('toc-wrapper');
-        if (slidebar.classList.contains('collapsed')) {
-            slidebar.classList.remove('collapsed');
-            const offset = slidebarWrapper.offsetWidth + 10;
-            contents.style.left = `${offset}px`;
-        } else {
-            slidebar.classList.add('collapsed');
-            const offset = slidebarWrapper.offsetWidth + 10;
-            contents.style.left = `${offset}px`;
-        }
+    if(toggle) {
+        toggle.addEventListener('click', function() {
+            const slidebar = document.querySelector('.toc-fixed');
+            if (slidebar.classList.contains('collapsed')) {
+                slidebar.classList.remove('collapsed');
+            } else {
+                slidebar.classList.add('collapsed');
+            }
+            limitContentWidth();
 
-    });
+        });
+    }
 }
 
