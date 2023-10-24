b72947a52222aa4b5c1ab40d3dc52201e9584697
Ognjen Maric <ognjen.maric@dfinity.org>
2023-10-24 14:15:36 +0200

    Use rsync instead of scp for deployment

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 JEKYLL_ENV=production bundle exec jekyll build
-scp -r _site/* ogi@lux:/var/www/html/blog
+# scp -r _site/* ogi@lux:/var/www/html/blog
+rsync -avz -e ssh _site/ ogi@lux:/var/www/html/blog
